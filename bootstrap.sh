2e6dcdd5512e0f301dd03d29c501c6ae9678ac75
James Kang <jkang@atlassian.com>
2016-05-18 12:14:17 -0400

    collecting apps that i usually install into a script

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install cask by updating homebrew
+brew update
+
+# install spectacle
+brew cask install spectacle
+
+# install iterm2
+brew cask install iterm2
+
+# install java
+brew cask install java
+
+# install evernote
+brew cask install evernote
+
+# install nvm
+brew install nvm
+
+# install sublime
+brew tap caskroom/versions
+brew cask install sublime-text3
+
+# install maven
+brew install maven
+
+# install gradle
+brew install gradle
